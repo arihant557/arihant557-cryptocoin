2f10f9ed54f21302ce643890988567c474f85ba5
arihant557 <5arihant.jain5@gmail.com>
2020-01-12 23:04:14 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import DataSearch from './Components/DataSearch'
+
+ class App extends Component{
+
+    render(){
+        return(
+        
+            <div className = "App">
+            <DataSearch />
+
+            </div>
+        )
+    }
+}
+
+export default App;
